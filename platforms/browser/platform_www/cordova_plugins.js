e756e662bbc8bba3f84b313d1208fdee9be1ed02
Alfredo AD <alfredo7891@gmail.com>
2016-05-31 16:20:36 -0500

    sadffsd

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -39,12 +39,29 @@ module.exports = [
         "clobbers": [
             "backgroundGeoLocation"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-mauron85-background-geolocation": "0.9.6"
+    "cordova-plugin-mauron85-background-geolocation": "0.9.6",
+    "cordova-plugin-console": "1.0.3",
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-splashscreen": "3.2.2"
 }
 // BOTTOM OF METADATA
 });
