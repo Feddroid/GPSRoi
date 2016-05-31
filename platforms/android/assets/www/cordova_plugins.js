48b46b5b83e79f708ea3cd7b2729e9e7b03c033b
Alfredo AD <alfredo7891@gmail.com>
2016-05-31 15:14:05 -0500

    sad

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeoLocation.js",
+        "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeoLocation",
+        "clobbers": [
+            "backgroundGeoLocation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-mauron85-background-geolocation": "0.9.6"
+};
+// BOTTOM OF METADATA
+});
